ab9817152e8b76e11fd9acf09720dac2407df06d
Hirano <hnaoya1120@gmail.com>
2023-07-19 23:02:22 +0900

    週と月の切り替え表示をuseStateに持たせる

--- components/calendarHeader.tsx
+++ components/calendarHeader.tsx
@@ -2,17 +2,25 @@ import styles from "./calendarHeader.module.css";
 import addMonths from "date-fns/addMonths";
 import format from "date-fns/format";
 import subMonths from "date-fns/subMonths";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 type CALENDER_HEADER = {
     setTargetDate: Dispatch<SetStateAction<Date>>;
     targetDate: Date;
+    setSwichDisplay: Dispatch<SetStateAction<"月" | "週">>;
+    switchDisplay: "月" | "週";
 };
 
 export const CalendarHeader = ({
     setTargetDate,
     targetDate,
+    setSwichDisplay,
 }: CALENDER_HEADER) => {
+    const dateArray: ("月" | "週")[] = ["月", "週"];
+    const handleChange = (e: any) => {
+        console.log(dateArray[e.target.value]);
+        setSwichDisplay(dateArray[e.target.value]);
+    };
     return (
         <header className={styles.headerContainer}>
             <h1 className={styles.calendarH1}>Calendar</h1>
@@ -38,6 +46,15 @@ export const CalendarHeader = ({
             <h2 className={styles.calendarH2}>
                 {format(targetDate, "y年M月")}
             </h2>
+            <div className={styles.displaySwitch}>
+                <select onChange={(e) => handleChange(e)} name="displaySwitch">
+                    {dateArray.map((date, key) => (
+                        <option key={key} value={key}>
+                            {date}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </header>
     );
 };
--- pages/posts/page1.tsx
+++ pages/posts/page1.tsx
@@ -1,20 +1,25 @@
 import { CalendarHeader } from "@/components/calendarHeader";
 import { Month } from "@/components/month";
 import { getFunsMonth } from "@/util";
+import styles from "../../styles/Home.module.css";
 import { useState } from "react";
 
 export default function Page1() {
     const [targetDate, setTargetDate] = useState<Date>(new Date());
+    const [switchDisplay, setSwichDisplay] = useState<"月" | "週">("月");
     let calendar = getFunsMonth(targetDate);
     return (
         <>
-            <div>
+            <div className={styles.container}>
                 <CalendarHeader
                     setTargetDate={setTargetDate}
                     targetDate={targetDate}
+                    setSwichDisplay={setSwichDisplay}
+                    switchDisplay={switchDisplay}
                 />
                 <div>
-                    <Month month={calendar}></Month>
+                    {switchDisplay === "月" && <Month month={calendar}></Month>}
+                    {switchDisplay === "週" && <div>週の表示です</div>}
                 </div>
             </div>
         </>

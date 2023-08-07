44140437d911f99e8b9ffce1be44298ae1048f08
Hirano <hnaoya1120@gmail.com>
2023-08-07 18:18:30 +0900

    曜日の共通部品を修正

--- util.ts
+++ util.ts
@@ -29,15 +29,15 @@ export const getEnglishWeek = (index: number) => {
     switch (index) {
         case 0:
             return "Sun";
-        case 2:
+        case 1:
             return "Mon";
-        case 3:
+        case 2:
             return "Tue";
-        case 4:
+        case 3:
             return "Wed";
-        case 5:
+        case 4:
             return "Thi";
-        case 6:
+        case 5:
             return "Fry";
         default:
             return "Sat";
